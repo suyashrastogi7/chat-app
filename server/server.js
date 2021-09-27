fb2d679b704e445f69d0b626ecb3685f2c258dc2
suyashrastogi7 <suyashrastogi@protonmail.com>
2021-09-28 03:06:59 +0530

    basic structure setup

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+// const { createServer } = require("http");
+const socketio = require("socket.io");
+require('dotenv').config();
+const PORT  = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'))
+
+
+const server = app.listen(5000);
+
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    socket.on('messageFromClient', (payload) => {
+      io.emit('messageFromServer',payload)
+    })
+})
